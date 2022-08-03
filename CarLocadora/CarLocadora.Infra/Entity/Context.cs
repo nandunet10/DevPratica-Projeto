f653ada3a1e02487e2144e863ab74c8b912afe68
Fernando Oliveira Santos <fernando.santos@susep.gov.br>
2022-08-02 22:44:31 -0300

    Criação da classe de contexto.

---
+++ CarLocadora/CarLocadora.Infra/Entity/Context.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CarLocadora.Infra.Entity
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options): base(options) { }
+
+
+    }
+}
