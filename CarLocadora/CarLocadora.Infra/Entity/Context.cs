94886c43360b1e7c8c396a429e68dccfa484ec28
Fernando Oliveira Santos <fernando.santos@susep.gov.br>
2022-08-03 10:07:09 -0300

    Criando as tabelas na abordagem codefirst

--- CarLocadora/CarLocadora.Infra/Entity/Context.cs
+++ CarLocadora/CarLocadora.Infra/Entity/Context.cs
@@ -1,4 +1,5 @@
 ﻿
+using CarLocadora.Modelo.Modelos;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarLocadora.Infra.Entity
@@ -7,6 +8,10 @@ namespace CarLocadora.Infra.Entity
     {
         public Context(DbContextOptions<Context> options): base(options) { }
 
-
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+        public DbSet<Veiculos> Veiculos { get; set; }
+        public DbSet<FormasDePagamento> FormasDePagamento { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
     }
 }
