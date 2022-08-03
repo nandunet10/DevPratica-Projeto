decaf6cbcdf4ae1d6bbc6ed1248d98a5eb372060
Fernando Oliveira Santos <fernando.santos@susep.gov.br>
2022-08-02 22:49:38 -0300

    Adição da string de conexão, e a referença do context na configuração de inicialização do sistema.

---
+++ CarLocadora/CarLocadora.API/Startup.cs
@@ -0,0 +1,55 @@
+﻿using CarLocadora.Infra.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace CarLocadora.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<Context>(options => options.UseSqlServer(connection));
+
+            //Adicionar Scoped
+            //services.AddScoped<Interface, Servico>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api de locadora de veículos", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api de locadora de veículos v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
