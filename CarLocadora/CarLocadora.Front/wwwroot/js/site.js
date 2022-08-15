396d98e6c70b67e9da2fd8d370c620597babdc12
Fernando Oliveira Santos <fernando.santos@susep.gov.br>
2022-08-15 19:36:47 -0300

    implementando mascara nos campos!

--- CarLocadora/CarLocadora.Front/wwwroot/js/site.js
+++ CarLocadora/CarLocadora.Front/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('.maskTelefone').inputmask({ mask: ['(99) 9999-9999'] });
+    $('.maskCelular').inputmask({ mask: ['(99) 99999-9999'] });
 
-// Write your JavaScript code.
+    $('.maskCPF').inputmask({ mask: ['999.999.999-99'] });
+    $('.maskCNPJ').inputmask({ mask: ['99.999.999/9999-99'] });
+
+    $('.maskValor').inputmask({mask: ['999999999999999.99']})
+
+})
